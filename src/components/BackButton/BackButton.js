277d085e31e2e1c167d81f93e27bddbcd07c478a
Sergey Kambalin <greyexpert@gmail.com>
2018-04-06 19:15:28 +0300

    Fix incorrect style notice

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import {
   Button,
-  Icon
+  Icon,
 } from 'native-base';
 
 import { withStyleSheet as styleSheet } from 'theme';
@@ -11,17 +11,13 @@ import { withStyleSheet as styleSheet } from 'theme';
     fontSize: 35,
     color: '#BDC0CB',
   },
-
-  button: {
-
-  }
 })
 export default class BackButton extends PureComponent {
   render() {
-    const { styleSheet, style, ...props } = this.props;
+    const { styleSheet, ...props } = this.props;
 
     return (
-      <Button rounded light transparent {...props} style={[style, styleSheet.button]}>
+      <Button rounded light transparent {...props}>
         <Icon style={styleSheet.icon} name={'ios-arrow-back'} />
       </Button>
     );
