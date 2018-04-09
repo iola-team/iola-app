012e96906789ea829dafc92194cceb1034e6d6a6
Sergey Kambalin <greyexpert@gmail.com>
2018-04-09 08:52:03 +0300

    Remove clipped list items

--- src/theme/default/components/index.js
+++ src/theme/default/components/index.js
@@ -129,7 +129,7 @@ export default (variables => ({
           justifyContent: 'center'
         },
         'NativeBase.Right': {
-          justifyContent: 'center'
+          justifyContent: 'center',
         },
       }
     }
