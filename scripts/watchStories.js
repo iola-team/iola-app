5767a199f9c5df60efdfec8727f086e97b0427a4
Sergey Kambalin <greyexpert@gmail.com>
2018-04-14 17:55:25 +0300

    Add stories watcher to auto generate story loader

---
+++ scripts/watchStories.js
@@ -0,0 +1,30 @@
+import { join } from 'path'
+import watch from 'glob-watcher';
+import { exec } from 'child-process-promise';
+
+const globs = [
+  join(__dirname, '..', 'src', '**/*.(stories.js)'),
+]
+
+const options = {
+  ignoreInitial: false,
+  events: [ 'add', 'unlink' ],
+};
+
+if (require.main === module) {
+  console.log('Start watching stories');
+
+  watch(globs, options, async (done) => {
+    console.log('Generating stories...');
+
+    try {
+      await exec(`yarn storybook:generate`);
+    } catch(e) {
+      console.error(e);
+    }
+
+    console.log('Storybook was generated!');
+
+    done();
+  });
+}
