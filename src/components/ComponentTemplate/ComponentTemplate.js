5780a241d9f82e2824472f2b20f0a256ef5324e1
Sergey Kambalin <greyexpert@gmail.com>
2018-04-25 10:50:33 +0300

    Add more examples to component template

--- src/components/ComponentTemplate/ComponentTemplate.js
+++ src/components/ComponentTemplate/ComponentTemplate.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { propType as fragmentProp } from 'graphql-anywhere';
+import gql from 'graphql-tag';
 import {
   View,
   Text,
+  Button,
 } from 'native-base';
 
 import { withStyleSheet as styleSheet, connectToStyleSheet } from 'theme/index';
+
+/**
+ * Component fragment sample
+ */
+const userFragment = gql`
+  fragment ComponentTemplate_user on User {
+    id
+  }
+`;
 
 const Root = connectToStyleSheet('root', View);
 
@@ -13,13 +26,42 @@ const Root = connectToStyleSheet('root', View);
 
   }
 })
-export default class componentTemplate extends Component {
+export default class ComponentTemplate extends Component {
+  /**
+   * Graphql fragments - can be used by parent components to fetch data
+   */
+  static fragments = {
+    user: userFragment,
+  }
+
+  static propTypes = {
+    /**
+     * Graphql props
+     */
+    user: fragmentProp(userFragment).isRequired,
+
+    /**
+     * Simple props
+     */
+    something: PropTypes.number,
+    onSomething: PropTypes.func,
+  };
+
+  static defaultProps = {
+    something: 0,
+    onSomething: () => {},
+  };
+
   render() {
-    const { style } = this.props;
+    const { style, onSomething, something, user } = this.props;
+
+    console.log('User', user);
 
     return (
       <Root style={style}>
-        <Text>Hello ComponentTemplate!</Text>
+        <Button onPress={onSomething}>
+          <Text>Hello ComponentTemplate = {something}!</Text>
+        </Button>
       </Root>
     );
   }
--- src/components/ComponentTemplate/ComponentTemplate.stories.js
+++ src/components/ComponentTemplate/ComponentTemplate.stories.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { withHandlers } from 'recompose';
+import { number, withKnobs } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
 
 import { getContentDecorator } from 'storybook/index';
-import componentTemplate from './ComponentTemplate';
+import ComponentTemplate from './ComponentTemplate';
 
 const stories = storiesOf('Components/ComponentTemplate', module);
 
 // Decorators
+stories.addDecorator(withKnobs);
 stories.addDecorator(getContentDecorator({ padder: true }));
 
+// Mock data
+const user = {
+  id: 123,
+};
+
 // Stories
-stories.add('Default', () => (
-  <componentTemplate />
-));
+stories.add('Default', () => {
+  const something = number('Some number', 0);
+
+  return (
+    <ComponentTemplate
+      user={user}
+      something={something}
+      onSomething={action('onSomething')}
+    />
+  );
+});
