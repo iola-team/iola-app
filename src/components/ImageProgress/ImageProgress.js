846b279d799a0aa62f0975da49fd19b18d3f0819
Sergey Kambalin <greyexpert@gmail.com>
2018-04-28 15:43:36 +0300

    Add image progress component

--- src/components/ImageProgress/ImageProgress.js
+++ src/components/ImageProgress/ImageProgress.js
@@ -1,37 +1,110 @@
 import React, { Component } from 'react';
+import { noop } from 'lodash';
 import PropTypes from 'prop-types';
-import { propType as fragmentProp } from 'graphql-anywhere';
-import gql from 'graphql-tag';
+import * as Animatable from 'react-native-animatable';
+import {
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
 import {
   View,
-  Text,
   Button,
+  Icon,
 } from 'native-base';
 
 import { withStyleSheet as styleSheet, connectToStyleSheet } from 'theme/index';
+import CircularProgress from '../CircularProgress';
 
 const Root = connectToStyleSheet('root', View);
+const Layer = connectToStyleSheet('layer', Animatable.View);
+const Progress = connectToStyleSheet('progress', CircularProgress);
+const Background = connectToStyleSheet('background', ImageBackground);
 
 @styleSheet('Sparkle.ImageProgress', {
   root: {
 
-  }
+  },
+
+  layer: {
+    ...StyleSheet.absoluteFillObject,
+  },
+
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  progress: {
+    size: 70,
+  },
 })
 export default class ImageProgress extends Component {
-  static propTypes = {
+  state = {
+    isVisible: null,
+  };
 
+  static propTypes = {
+    previewUrl: PropTypes.string.isRequired,
+    progress: PropTypes.number,
+    onCancel: PropTypes.func,
+    active: PropTypes.bool,
   };
 
   static defaultProps = {
-
+    onCancel: noop,
+    progress: 0,
+    active: false,
   };
 
+  static getDerivedStateFromProps({ active }, { isVisible }) {
+    return {
+      isVisible: isVisible === null ? active : isVisible,
+    };
+  }
+
+  onAnimationEnd() {
+    this.setState({
+      isVisible: this.props.active,
+    });
+  }
+
   render() {
-    const { style, children } = this.props;
+    const {
+      style,
+      children,
+      progress,
+      previewUrl,
+      active,
+      onCancel,
+    } = this.props;
+
+    const { isVisible } = this.state;
 
     return (
       <Root style={style}>
         {children}
+
+        {
+          isVisible && (
+            <Layer
+              easing="ease-out"
+              transition="opacity"
+              duration={200}
+              onAnimationEnd={::this.onAnimationEnd}
+              style={{ opacity: active ? 1 : 0 }}
+            >
+              <Background source={{ uri: previewUrl }} blurRadius={5}>
+                <Progress progress={progress}>
+                  <Button block light transparent rounded onPress={onCancel}>
+                    <Icon name={'md-close'} />
+                  </Button>
+                </Progress>
+              </Background>
+            </Layer>
+          )
+        }
+
       </Root>
     );
   }
--- src/components/ImageProgress/ImageProgress.stories.js
+++ src/components/ImageProgress/ImageProgress.stories.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withHandlers } from 'recompose';
-import { number, withKnobs } from '@storybook/addon-knobs/react';
+import { number, boolean, withKnobs } from '@storybook/addon-knobs/react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
 import { Image } from 'react-native';
+import { Button, View, Text } from 'native-base';
 
 import { getContentDecorator } from 'storybook/index';
 import ImageProgress from './ImageProgress';
@@ -14,18 +15,36 @@ const stories = storiesOf('Components/ImageProgress', module);
 stories.addDecorator(withKnobs);
 stories.addDecorator(getContentDecorator({ padder: true }));
 
+const getProgress = () => number('Progress', 0.3, {
+  range: true,
+  min: 0,
+  max: 1,
+  step: 0.01,
+})
+
 const imageUrl = 'http://endlesstheme.com/Endless1.5.1/img/user2.jpg';
 const imageStyles = {
   width: 150,
   height: 150,
 };
 
+const progressStyle = {
+  alignSelf: 'flex-start',
+};
+
 // Stories
 stories.add('Default', () => {
-  const something = number('Some number', 0);
+  const progress = getProgress();
+  const active = boolean('Active', true);
 
   return (
-    <ImageProgress progress={0.5}>
+    <ImageProgress
+      previewUrl={imageUrl}
+      active={active}
+      progress={progress}
+      style={progressStyle}
+      onCancel={action('onCancel')}
+    >
       <Image style={imageStyles} source={{ uri: imageUrl }} />
     </ImageProgress>
   );
