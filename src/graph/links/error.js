f14b3bd8779f790b20700d6d59c1cec5876fb69e
Roman Romanovsky <rromanovsky@klika-tech.com>
2018-05-05 12:39:20 +0300

    Added apollo-link-error

--- src/graph/links/error.js
+++ src/graph/links/error.js
@@ -2,10 +2,10 @@ import { onError } from 'apollo-link-error';
 
 export default () => onError(({ networkError, graphQLErrors }) => {
   if (graphQLErrors) {
-    graphQLErrors.map(({ message, locations, path }) => console.log(
-      `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+    graphQLErrors.map(({ message, locations, path }) => console.error(
+      `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`,
     ));
   }
 
-  if (networkError) alert(`[Network error]: ${networkError}`);
+  if (networkError) console.error(`[Network error]: ${networkError}`);
 });
