da06a5bc4d727816e57d7da92431b26d7dffd397
Roman Romanovsky <rromanovsky@klika-tech.com>
2018-05-10 11:43:33 +0300

    WIP: email validation

--- src/screens/SignUp/SignUpForm.js
+++ src/screens/SignUp/SignUpForm.js
@@ -4,10 +4,18 @@ import { withFormik } from 'formik';
 import yup from 'yup';
 import { Button, Form, Input, Item, Text } from 'native-base';
 import { withStyleSheet as styleSheet, connectToStyleSheet } from 'theme';
-import { graphql, Mutation } from 'react-apollo';
+import { graphql, Mutation, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import { LAUNCH } from '../roteNames';
+
+const validateEmailQuery = gql`
+  query validateEmailQuery($email: String = "") {
+    users(email: $email) {
+      totalCount
+    }
+  }
+`;
 
 const signUpUserMutation = gql`
   mutation signUpUserMutation($input: SignUpUserInput!) {
@@ -54,6 +62,9 @@ const SubmitButton = connectToStyleSheet('submitButton', Button).withProps({ blo
   },
 })
 class SignUpForm extends Component {
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  // }
+
   async onSubmit(signUpUser) {
     const { values: { name, email, password }, storeToken } = this.props;
     let success = false;
@@ -72,7 +83,9 @@ class SignUpForm extends Component {
       await storeToken(result.accessToken);
       success = !!result.accessToken;
     } catch (error) {
-      if (error.message.includes('Duplicate email')) alert('This email is already taken');
+      if (_.find(error.graphQLErrors, { message: 'Duplicate email' })) {
+        alert('This email is already taken');
+      }
     }
 
     if (success) this.props.navigation.navigate(LAUNCH);
@@ -96,6 +109,17 @@ class SignUpForm extends Component {
             value={values.name}
           />
         </FormItem>
+        <Query
+          query={validateEmailQuery}
+          variables={{ email: values.email }}
+        >
+          {({ loading, error, data, startPolling, stopPolling }) => {
+            if (loading) return <Text>LOADING</Text>;
+            if (error) alert(`Error: ${JSON.stringify(error)}`);
+
+            return <Text>Count: {data.users.totalCount}, {values.email}</Text>;
+          }}
+        </Query>
         <FormItem>
           <FormInput
             placeholder="Email"
