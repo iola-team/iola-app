925857fdc0302947c568976101df53ef09b57fcc
Roman Romanovsky <rromanovsky@klika-tech.com>
2018-06-08 10:05:17 +0300

    Photo Preview footer markup

--- src/components/PhotoPreview/PhotoPreview.js
+++ src/components/PhotoPreview/PhotoPreview.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Text, View } from 'react-native';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import FoundationIcon from 'react-native-vector-icons/Foundation';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import ExtraDimensions from 'react-native-extra-dimensions-android';
 
@@ -9,6 +11,11 @@ import { withStyleSheet as styleSheet, connectToStyleSheet } from 'theme';
 
 const Indicator = connectToStyleSheet('indicator', Text);
 const Footer = connectToStyleSheet('footer', View);
+const Title = connectToStyleSheet('title', Text);
+const DateTime = connectToStyleSheet('dateTime', Text);
+const RightBlock = connectToStyleSheet('rightBlock', View);
+const ShareButton = connectToStyleSheet('footerButton', IoniconsIcon).withProps({ name: 'ios-share-alt' });
+const DeleteButton = connectToStyleSheet('footerButton', FoundationIcon).withProps({ name: 'trash' });
 
 @styleSheet('Sparkle.PhotoPreview', {
   indicator: {
@@ -24,9 +31,45 @@ const Footer = connectToStyleSheet('footer', View);
   },
 
   footer: {
+    marginHorizontal: 17,
+    marginBottom: 31,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: '#CC0000',
+  },
+
+  title: {
+    paddingBottom: 6,
+    fontFamily: 'SF Pro Text',
+    fontSize: 16,
+    fontWeight: '600',
+    letterSpacing: -0.06,
+    lineHeight: 19,
+    color: '#FFFFFF',
+  },
+
+  dateTime: {
+    fontFamily: 'SF Pro Text',
+    fontSize: 14,
+    lineHeight: 17,
+    color: '#BDC0CB',
+  },
+
+  rightBlock: {
+    width: (24 * 2 + 14),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  footerButton: {
+    height: 24,
+    width: 24,
+    borderWidth: 1.5,
+    borderColor: 'rgba(189, 192, 203, 0.25)',
+    borderRadius: 8,
+    textAlign: 'center',
+    lineHeight: 24,
+    fontSize: 14,
+    color: '#BDC0CB',
   },
 })
 export default class PhotoPreview extends Component {
@@ -53,19 +96,20 @@ export default class PhotoPreview extends Component {
   }
 
   renderIndicator(currentIndex, allSize) {
-    return <Indicator>{`${currentIndex} / ${allSize}`}</Indicator>;
+    return allSize > 1 ? <Indicator>{`${currentIndex} of ${allSize}`}</Indicator> : null;
   }
 
   renderFooter() {
     return (
       <Footer>
         <View>
-          <Text style={{ color: '#FFFFFF' }}>Natalie Rose</Text>
-          <Text style={{ color: '#FFFFFF' }}>Today at 18:16</Text>
+          <Title>Natalie Rose</Title>
+          <DateTime>Today at 18:16</DateTime>
         </View>
-        <View>
-          <Text style={{ color: '#FFFFFF' }}>Delete</Text>
-        </View>
+        <RightBlock>
+          <ShareButton onPress={() => alert('Share')} />
+          <DeleteButton onPress={() => alert('Delete')} />
+        </RightBlock>
       </Footer>
     );
   }
@@ -95,8 +139,10 @@ export default class PhotoPreview extends Component {
             renderIndicator={this.renderIndicator}
             renderFooter={this.renderFooter}
             footerContainerStyle={footerContainerStyle}
-            imageUrls={photos}
             index={index}
+            imageUrls={photos}
+            failImageSource={{ uri: '@TODO' }}
+            loadingRender={() => null /* '@TODO' */}
             onSwipeDown={::this.onClose}
             backgroundColor="rgba(46, 48, 55, 0.95)"
           />
