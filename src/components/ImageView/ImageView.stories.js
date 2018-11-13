5d7d768b082266fccf29bbdecab2739f89639948
Roman Romanovsky <roman.romanovsky.work@gmail.com>
2018-11-13 15:47:24 +0300

    ImageView story: add query for photos

--- src/components/ImageView/ImageView.stories.js
+++ src/components/ImageView/ImageView.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, View } from 'react-native';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { storiesOf } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
@@ -19,22 +20,15 @@ const typeDefs = gql`
   scalar Date
 
   type Query {
+    me: User!
     node(id: ID!): Photo!
-  }
-
-  type Photo {
-    id: ID!
-    url: String!
-    caption: String!
-    createdAt: Date!
-    user: User!
-    comments(first: Int, after: Cursor, last: Int, before: Cursor): PhotoCommentsConnection!
   }
 
   type User {
     id: ID!
     name: String!
     avatar: Avatar!
+    photos(first: Int, after: Cursor, last: Int, before: Cursor): UserPhotoConnection!
   }
 
   type Avatar {
@@ -48,22 +42,32 @@ const typeDefs = gql`
     BIG
   }
 
+  type UserPhotoConnection {
+    pageInfo: PageInfo!
+    metaInfo: ConnectionMetaInfo!
+    edges: [PhotoEdge!]!
+    totalCount: Int
+  }
+
+  type PhotoEdge {
+    node: Photo!
+    cursor: Cursor!
+  }
+  
+  type Photo {
+    id: ID!
+    url: String!
+    caption: String!
+    createdAt: Date!
+    user: User!
+    comments(first: Int, after: Cursor, last: Int, before: Cursor): PhotoCommentsConnection!
+  }
+
   type PhotoCommentsConnection {
     pageInfo: PageInfo!
     metaInfo: ConnectionMetaInfo!
     edges: [CommentEdge!]!
     totalCount: Int
-  }
-
-  type PageInfo {
-    hasNextPage: Boolean!
-    hasPreviousPage: Boolean!
-    startCursor: Cursor
-    endCursor: Cursor
-  }
-
-  type ConnectionMetaInfo {
-    firstCursor: Cursor!
   }
 
   type CommentEdge {
@@ -76,6 +80,44 @@ const typeDefs = gql`
     text: String!
     createdAt: Date!
     user: User!
+  }
+
+  type PageInfo {
+      hasNextPage: Boolean!
+      hasPreviousPage: Boolean!
+      startCursor: Cursor
+      endCursor: Cursor
+  }
+
+  type ConnectionMetaInfo {
+      firstCursor: Cursor!
+  }
+`;
+
+const userPhotosQuery = gql`
+  query userPhotosQuery {
+    user: me {
+      ...on User {
+        photos {
+          edges {
+            node {
+              ...on Photo {
+                id
+                url
+                caption
+                createdAt
+                user {
+                  ...on User {
+                    id
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 `;
 
@@ -95,6 +137,8 @@ const dataStore = (() => {
   const photos = shuffle(['https://newbor.by/upload/iblock/dbf/dom_1.jpg', 'https://newbor.by/upload/resize_cache/iblock/ded/1024_768_040cd750bba9870f18aada2478b24840a/dom_2.jpg', 'https://newbor.by/upload/iblock/5dd/dom_3.jpg', 'https://newbor.by/upload/iblock/759/dom_4.jpg', 'https://newbor.by/upload/resize_cache/iblock/995/1024_768_040cd750bba9870f18aada2478b24840a/dom_5.jpg', 'https://newbor.by/upload/resize_cache/iblock/d5c/1024_768_040cd750bba9870f18aada2478b24840a/dom_8.14.jpg', 'https://newbor.by/upload/resize_cache/iblock/1a9/1024_768_040cd750bba9870f18aada2478b24840a/dom_11.jpg', 'https://newbor.by/upload/iblock/13f/dom_8.jpg', 'https://newbor.by/upload/resize_cache/iblock/5d7/1024_768_040cd750bba9870f18aada2478b24840a/dom_10.jpg', 'https://newbor.by/upload/iblock/0f1/dom_13.jpg', 'https://newbor.by/upload/iblock/52d/6.6-min.jpg', 'https://newbor.by/upload/iblock/42d/6.3.jpg', 'https://newbor.by/upload/iblock/861/6.6.jpg', 'https://newbor.by/upload/iblock/7f2/detskiy-sad-7.jpg', 'https://newbor.by/upload/resize_cache/iblock/953/1024_768_040cd750bba9870f18aada2478b24840a/detskiy-sad-8.jpg', 'https://newbor.by/upload/iblock/55a/detskiy-sad-6.jpg', 'https://newbor.by/upload/iblock/951/detskiy-sad-1.jpg', 'https://newbor.by/upload/resize_cache/iblock/870/1024_768_040cd750bba9870f18aada2478b24840a/detskiy-sad-9.jpg', 'https://newbor.by/upload/resize_cache/iblock/0ff/1024_768_040cd750bba9870f18aada2478b24840a/fontan-v-novoy-borovoy-_-obshchiy-plan-_2.jpg', 'https://newbor.by/upload/resize_cache/iblock/3f5/1024_768_040cd750bba9870f18aada2478b24840a/detskiy-sad-kedrovogo-kvartala.jpg', 'https://newbor.by/upload/resize_cache/iblock/421/1024_768_040cd750bba9870f18aada2478b24840a/obshchestvennye-prostranstva.jpg', 'https://newbor.by/upload/resize_cache/iblock/7b0/1024_768_040cd750bba9870f18aada2478b24840a/veloboksy-vo-dvorakh-novoy-borovoy.jpg', 'https://newbor.by/upload/iblock/549/dji_0234.jpg', 'https://newbor.by/upload/iblock/8b6/dji_0286.jpg', 'https://newbor.by/upload/iblock/595/shkola_2.png', 'https://newbor.by/upload/iblock/6a8/shkola_1.png', 'https://newbor.by/upload/iblock/9cd/shkola_4.png', 'https://newbor.by/upload/iblock/cff/shkola_5.png', 'https://newbor.by/upload/iblock/689/shkola_6.png', 'https://newbor.by/upload/iblock/d34/shkola_7.png', 'https://newbor.by/upload/resize_cache/iblock/6b8/1024_768_040cd750bba9870f18aada2478b24840a/shkola_01.png', 'https://newbor.by/upload/iblock/b25/shkola_3-_2_.png', 'https://newbor.by/upload/resize_cache/iblock/a80/1024_768_040cd750bba9870f18aada2478b24840a/shkola_0.png', 'https://newbor.by/upload/iblock/cfe/shkola_8.png', 'https://newbor.by/upload/resize_cache/iblock/70e/1024_768_040cd750bba9870f18aada2478b24840a/vezd-v-novuyu-borovuyu.jpg']);
 
   return {
+    users,
+
     photos: Array.from({ length: photos.length }).map((item, index) => ({
       id: `Photo:${index + 1}`,
       url: photos[index],
@@ -117,31 +161,33 @@ const dataStore = (() => {
 
 const resolvers = {
   Query: {
+    me: (root, args, { dataStore }) => dataStore.users[0],
     node: (root, { id }, { dataStore }) => find(dataStore.photos, { id }),
   },
 
+  User: {
+    photos(user, args, { dataStore: { photos } }) {
+      return createConnection(photos, args);
+    },
+  },
+
   Photo: {
-//     async comments(photo, args) {
-// console.log('args',args);
-//       const comments = orderBy(photo.comments, 'createdAt');
-//
-//       return createConnection(comments, args);
-//     },
+    comments: (photo, args) => {
+      const comments = orderBy(photo.comments, 'createdAt');
 
-    comments: (photo, { first }) => {
-      const comments = orderBy(photo.comments, 'createdAt').slice(0, first);
-
-      return {
-        totalCount: comments.length,
-        edges: comments.map((comment, index) => ({
-          cursor: `cursor:${index}`,
-          node: { ...comment },
-        })),
-        pageInfo: {
-          hasNextPage: true,
-          endCursor: 'cursor:20', // @TODO: hmmm
-        },
-      };
+      return createConnection(comments, args);
+      // @TODO: hmmm
+      // return {
+      //   totalCount: comments.length,
+      //   edges: comments.map((comment, index) => ({
+      //     cursor: `cursor:${index}`,
+      //     node: { ...comment },
+      //   })),
+      //   pageInfo: {
+      //     hasNextPage: true,
+      //     endCursor: 'cursor:20',
+      //   },
+      // };
     },
   },
 };
@@ -168,16 +214,27 @@ stories.add('Default', () => {
   };
 
   return (
-    <ImageView images={dataStore.photos}>
-      {onShowImage => (
-        <View style={styles.view}>
-          {dataStore.photos.map(({ id, url }, index) => (
-            <TouchableOpacity key={id} onPress={() => onShowImage(index)}>
-              <Image source={{ uri: url }} style={styles.image} />
-            </TouchableOpacity>
-          ))}
-        </View>
-      )}
-    </ImageView>
+    <Query query={userPhotosQuery}>
+      {({ loading, data }) => {
+        if (loading) return null;
+
+        const { user: { photos: { edges } } } = data;
+        const photos = edges.map(edge => ({ ...edge.node }));
+
+        return (
+          <ImageView images={photos}>
+            {onShowImage => (
+              <View style={styles.view}>
+                {photos.map(({ id, url }, index) => (
+                  <TouchableOpacity key={id} onPress={() => onShowImage(index)}>
+                    <Image source={{ uri: url }} style={styles.image} />
+                  </TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </ImageView>
+        );
+      }}
+    </Query>
   );
 });
