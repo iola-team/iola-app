ea8a7aed1a17e07e53b9b221ca4092794f08ae84
Sergey Kambalin <greyexpert@gmail.com>
2019-02-05 16:32:49 +0600

    Privacy policy and terms of use buttons

--- src/components/SettingList/SettingList.js
+++ src/components/SettingList/SettingList.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { Text } from 'native-base';
+import { Linking } from 'react-native';
+import { Text, List, ListItem, Icon } from 'native-base';
 
 import { ScrollView } from '../TabNavigator';
 
 export default class SettingList extends Component {
+  openUrl = url => () => Linking.openURL(url);
+
   render() {
     const { ...props } = this.props;
 
+    /**
+     * TODO: retreave this urls from website configs
+     */
+    const termsUrl = 'http://demo.oxpro.org/oxwall/terms-of-use';
+    const policyUrl = 'http://demo.oxpro.org/oxwall/privacy-policy';
+
     return (
       <ScrollView {...props}>
-        <Text>Settings List</Text>
+        <List>
+          <ListItem button first onPress={this.openUrl(policyUrl)}>
+            <Text>Privacy policy</Text>
+          </ListItem>
+
+          <ListItem button last onPress={this.openUrl(termsUrl)}>
+            <Text>Terms of use</Text>
+          </ListItem>
+        </List>
       </ScrollView>
     );
   }
