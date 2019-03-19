d266773861685ed1008e36abcc5e5cc48bb8e771
Sergey Kambalin <greyexpert@gmail.com>
2019-03-19 15:17:39 +0600

    Fade animation placeholder

--- src/components/Placeholder/Placeholder.js
+++ src/components/Placeholder/Placeholder.js
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
-import LinearGradient from 'react-native-linear-gradient';
 import PropTypes from 'prop-types';
-import { View, StyleSheet, Animated } from 'react-native';
+import { View } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 import { withStyle } from '~theme';
 
-const Gradient = Animated.createAnimatedComponent(LinearGradient);
-const animatedValue = new Animated.Value(0);
-let animation = null;
-let componentsCount = 0;
-
-const retainAnimation = () => {
-  animation = animation || Animated.loop(
-    Animated.timing(animatedValue, {
-      toValue: 1,
-      duration: 2000,
-      isInteraction: false,
-      useNativeDriver: true,
-    }),
-  );
-
-  if (componentsCount === 0) {
-    animation.start();
-  }
-
-  componentsCount++;
-};
-
-const releaseAnimation = () => {
-  componentsCount--;
-
-  if (componentsCount <= 0) {
-    animation.stop();
-    componentsCount = 0;
-  }
-};
-
-@withStyle('Sparkle.Placeholder')
+@withStyle('Sparkle.Placeholder', {
+  overflow: 'hidden',
+})
 export default class Placeholder extends Component {
   static propTypes = {
     isActive: PropTypes.bool,
@@ -46,58 +17,24 @@ export default class Placeholder extends Component {
     isActive: true,
   };
 
-  state = {
-    layout: null,
-  };
-
-  onLayout = ({ nativeEvent: { layout } }) => {
-    this.setState({ layout });
-  };
-
-  componentDidMount() {
-    retainAnimation();
-  }
-
-  componentWillUnmount() {
-    releaseAnimation();
-  }
-
   render() {
-    const { style, children, isActive, ...props } = this.props;
-    const { layout } = this.state;
+    const { isActive, ...props } = this.props;
 
-    const animationStyle = layout && {
-      transform: [
-        {
-          translateX: animatedValue.interpolate({
-            inputRange: [0.5, 1],
-            outputRange: [-layout.width, layout.width],
-          }),
-        },
-      ],
-    };
+    return !isActive
+      ? <View {...props} />
+      : (
+        <Animatable.View
+          {...props}
+          useNativeDriver
+          animation={{
+            0: { opacity: 1 },
+            0.5: { opacity: 0.3 },
+            1: { opacity: 1 },
+          }}
 
-    return (
-      <View
-        {...props}
-        style={[style, { overflow: 'hidden' }]}
-        onLayout={this.onLayout}
-      >
-        {children}
-
-        {layout && isActive && (
-          <Gradient
-            style={[StyleSheet.absoluteFill, animationStyle]}
-            start={{x: 0, y: 0}}
-            end={{x: 1, y: 0}}
-            colors={[
-              'rgba(255, 255, 255, 0)',
-              'rgba(255, 255, 255, 0.8)',
-              'rgba(255, 255, 255, 0)',
-            ]}
-          />
-        )}
-      </View>
-    );
+          duration={3000}
+          iterationCount={Infinity}
+        />
+      );
   }
 }
