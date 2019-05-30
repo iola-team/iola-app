58909f48f1ff3b8175c49108f0c7d6d34ec8186b
Sergey Kambalin <greyexpert@gmail.com>
2019-05-30 12:31:27 +0600

    Error screen redesign

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View as ViewRN } from 'react-native';
-import { Text, Button } from 'native-base';
+import { View, Text, H1, Button } from 'native-base';
 
 import { withStyle } from '~theme';
+import imageSource from './image.png';
+import Image from '../Image';
 
 @withStyle('Sparkls.ErrorBoundary', {
   flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginHorizontal: 30,
+
+  'NativeBase.ViewNB': {
+    alignItems: 'center',
+
+    '.topSection': {
+      marginTop: 130,
+
+      'NativeBase.H1': {
+        textAlign: 'center',
+        marginBottom: 40,
+      },
+    },
+
+    '.bottomSection': {
+      marginBottom: 50,
+
+      'NativeBase.Text': {
+        textAlign: 'center',
+        marginBottom: 25,
+      },
+    },
+
+    'Sparkle.Image': {
+      width: 56.6,
+      height: 54,
+      marginBottom: 20,
+    },
+
+    'NativeBase.Button': {
+      marginHorizontal: 50,
+    },
+  },
 
   'NativeBase.Text': {
     fontSize: 24,
@@ -23,6 +57,7 @@ export default class ErrorBoundary extends Component {
   static propTypes = {
     onError: PropTypes.func,
     onRequestRelaunch: PropTypes.func.isRequired,
+    onReportPress: PropTypes.func.isRequired,
     children: PropTypes.element.isRequired,
   };
 
@@ -49,6 +84,13 @@ export default class ErrorBoundary extends Component {
     onRequestRelaunch(error);
   };
 
+  onReportPress = () => {
+    const { onReportPress } = this.props;
+    const { error } = this.state;
+
+    onReportPress(error);
+  };
+
   componentDidCatch(error) {
     this.setState({ error, isErrorHandled: false });
   }
@@ -63,10 +105,23 @@ export default class ErrorBoundary extends Component {
 
     return (
       <ViewRN {...props}>
-        <Text>Oops...</Text>
-        <Button onPress={this.onRequestRelaunch}>
-          <Text>Relaunch App</Text>
-        </Button>
+        <View topSection>
+          <Image source={imageSource} />
+          <H1>Oops! Something went wrong</H1>
+          <Button block onPress={this.onRequestRelaunch}>
+            <Text>Relaunch the apllication</Text>
+          </Button>
+        </View>
+        <View bottomSection>
+          <Text>
+            Or if you currently have free time and if you are a warm-hearted person 
+            you can leave a ticket in our Bug tracker
+          </Text>
+
+          <Button block secondary onPress={this.onReportPress}>
+            <Text>Leave a ticket</Text>
+          </Button>
+        </View>
       </ViewRN>
     );
   }
