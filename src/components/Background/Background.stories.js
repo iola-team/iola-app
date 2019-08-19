e78bccfa88560a8abe5d93e23322830031d8ffcd
Roman Romanovsky <roman.romanovsky.work@gmail.com>
2019-08-19 20:15:59 +0300

    Merge remote-tracking branch 'remotes/origin/master' into config-on-launch-screen
    # Conflicts:
    #	src/screens/SignIn/SignIn.js

--- src/components/Background/Background.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react-native';
-
-import Background from './Background';
-
-const stories = storiesOf('Components/Background', module);
-
-// Stories
-stories.add('Default', () => <Background />);
