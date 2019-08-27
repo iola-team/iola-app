f0d393c5587f65c1576f77f96328a81d42a61aab
Sergey Kambalin <greyexpert@gmail.com>
2019-08-27 17:31:10 +0600

    Update all deps which can be updated

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,4 +1,5 @@
-import { graphql, compose } from 'react-apollo';
+import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Screen from './SignIn';
